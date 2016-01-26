1778f915c7103b7da0d36559b63e529521f38f55
MAC1 <MAC1@10.30.18.47>
2016-01-26 15:13:41 -0600

    First Commit

---
+++ src/assignment1/Assignment1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+/**
+ *
+ * @author MAC1
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     *
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**
+         * Assignment 1 
+         * Mythological: 
+         * 1. Barbarian<Ground Attacker<Attacker/Troop<Clash of Clan Character<Game
+         * Character<Character<Mythological Thing<Thing
+         *
+         * 2. Charlie<Unicorn<Mythological creature<Stupid creature<creature<being
+         * <Mythological thing<thing
+         *
+         * 3. Santa<Mythological person<fake person<person<being<mythological thing<thing
+         *
+         * Real:
+         *
+         * 1. Droid Turbo<Droid<Smart Phone<Phone<Electronic Device<Device<Thing
+         *
+         * 2. Porsche Roadster<Porsche<German Sports Car<Sports
+         * Car<Vehicle<thing
+         *
+         * 3. Dude<Chad’s Dog<Mutt/Dog<Dog<mammal<animal<being<thing
+         */
+
+    }
+
+}
